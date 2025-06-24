1195259614ffa47fbdb5a112140d9caa02531fcc
arnoldswang <arnold0121@gmail.com>
2025-06-24 15:22:42 +0200

    fix: updated unit tests

--- PipelineLogViewer.Tests/PipelineParserTests.cs
+++ PipelineLogViewer.Tests/PipelineParserTests.cs
@@ -1,74 +1,20 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 using FluentAssertions;
-using Xunit;
 
 namespace PipelineLogViewer.Tests;
 
 public class PipelineParserTests
 {
-    // We'll extract the parsing logic to make it more testable
     private readonly PipelineParser _parser = new();
 
     [Fact]
-    public void ParseLogs_WithValidInput_ShouldReturnFormattedOutput()
+    public void ParseLogs_ShouldReconstructPipelineInReverseOrder()
     {
-        // Arrange
+        // Arrange: 1 -> 2 -> -1; reversed: 2, 1, 0
         var input = """
-            pipeline1 msg1 0 [Hello World] msg2
-            pipeline1 msg2 0 [How are you?] END
-            """;
-
-        // Act
-        var result = _parser.ParseLogs(input);
-
-        // Assert
-        result.Should().Contain("Pipeline pipeline1");
-        result.Should().Contain("msg1| Hello World");
-        result.Should().Contain("msg2| How are you?");
-    }
-
-    [Fact]
-    public void ParseLogs_WithHexEncoding_ShouldDecodeCorrectly()
-    {
-        // Arrange
-        var hexMessage = Convert.ToHexString(Encoding.ASCII.GetBytes("Hello"));
-        var input = $"pipeline1 msg1 1 [{hexMessage}] END";
-
-        // Act
-        var result = _parser.ParseLogs(input);
-
-        // Assert
-        result.Should().Contain("msg1| Hello");
-    }
-
-    [Fact]
-    public void ParseLogs_WithMultiplePipelines_ShouldSeparateThem()
-    {
-        // Arrange
-        var input = """
-            pipeline1 msg1 0 [First pipeline] END
-            pipeline2 msg1 0 [Second pipeline] END
-            """;
-
-        // Act
-        var result = _parser.ParseLogs(input);
-
-        // Assert
-        result.Should().Contain("Pipeline pipeline1");
-        result.Should().Contain("Pipeline pipeline2");
-        result.Should().Contain("First pipeline");
-        result.Should().Contain("Second pipeline");
-    }
-
-    [Fact]
-    public void ParseLogs_WithComplexChain_ShouldPreserveOrder()
-    {
-        // Arrange
-        var input = """
-            pipeline1 msg3 0 [Third] END
-            pipeline1 msg1 0 [First] msg2
-            pipeline1 msg2 0 [Second] msg3
+            1 0 0 [Start] 1
+            1 2 0 [End] -1
+            1 1 0 [Middle] 2
             """;
 
         // Act
@@ -76,107 +22,123 @@ public class PipelineParserTests
 
         // Assert
         var lines = result.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-        var messageLines = lines.Where(l => l.Contains('|')).ToArray();
-        
-        messageLines[0].Should().Contain("msg1| First");
-        messageLines[1].Should().Contain("msg2| Second");
-        messageLines[2].Should().Contain("msg3| Third");
+        lines.Should().ContainInOrder(
+            "Pipeline 1",
+            "  2| End",
+            "  1| Middle",
+            "  0| Start"
+        );
     }
 
     [Fact]
-    public void ParseLogs_WithInvalidEncoding_ShouldHandleGracefully()
+    public void ParseLogs_ShouldDecodeHexAndReverseOrder()
     {
         // Arrange
-        var input = "pipeline1 msg1 99 [Invalid] END";
+        var hexBody = Convert.ToHexString(Encoding.ASCII.GetBytes("OK"));
+        var input = $"""
+            2 3 1 [{hexBody}] -1
+            2 2 1 [{hexBody}] 3
+            """;
 
         // Act
         var result = _parser.ParseLogs(input);
 
         // Assert
+        result.Should().Contain("Pipeline 2");
+        result.Should().Contain("  3| OK");
+        result.Should().Contain("  2| OK");
+    }
+
+    [Fact]
+    public void ParseLogs_WithMultiplePipelines_ShouldHandleEachSeparately()
+    {
+        var input = """
+            1 0 0 [one] -1
+            2 0 0 [two] -1
+            """;
+
+        var result = _parser.ParseLogs(input);
+
+        result.Should().Contain("Pipeline 1");
+        result.Should().Contain("Pipeline 2");
+        result.Should().Contain("  0| one");
+        result.Should().Contain("  0| two");
+    }
+
+    [Fact]
+    public void ParseLogs_WithMalformedLines_ShouldSkipThem()
+    {
+        var input = """
+            1 0 0 [Good line] -1
+            not a valid log line
+            2 0 0 [Also good] -1
+            """;
+
+        var result = _parser.ParseLogs(input);
+
+        result.Should().Contain("Pipeline 1");
+        result.Should().Contain("Pipeline 2");
+        result.Should().Contain("  0| Good line");
+        result.Should().Contain("  0| Also good");
+    }
+
+    [Fact]
+    public void ParseLogs_WithMissingTerminalNode_ShouldNotIgnoreIncompleteChains()
+    {
+        var input = """
+            1 0 0 [Start] 1
+            1 1 0 [Middle] 2
+            """;
+
+        var result = _parser.ParseLogs(input);
+        result.Should().Contain("Pipeline 1");
+    }
+
+    [Fact]
+    public void ParseLogs_ShouldHandleUUIDIdentifiers()
+    {
+        var input = """
+            1 a 0 [First] b
+            1 b 0 [Second] c
+            1 c 0 [Third] -1
+            """;
+
+        var result = _parser.ParseLogs(input);
+
+        result.Should().Contain("  c| Third");
+        result.Should().Contain("  b| Second");
+        result.Should().Contain("  a| First");
+    }
+
+    [Fact]
+    public void ParseLogs_WithInvalidEncoding_ShouldShowError()
+    {
+        var input = "1 x 9 [some body] -1";
+
+        var result = _parser.ParseLogs(input);
+
         result.Should().Contain("Invalid encoding");
     }
 
-    [Fact]
-    public void ParseLogs_WithEmptyInput_ShouldReturnEmptyResult()
+    [Theory]
+    [InlineData("4F4B", "OK")]
+    [InlineData("54657374206D7367", "Test msg")]
+    public void ParseLogs_WithHexBody_ShouldDecodeProperly(string hex, string expected)
     {
-        // Arrange
+        var input = $"2 1 1 [{hex}] -1";
+
+        var result = _parser.ParseLogs(input);
+
+        result.Should().Contain($"  1| {expected}");
+    }
+
+    [Fact]
+    public void ParseLogs_WithEmptyInput_ShouldReturnEmpty()
+    {
         var input = "";
 
-        // Act
         var result = _parser.ParseLogs(input);
 
-        // Assert
         result.Should().BeEmpty();
     }
-
-    [Fact]
-    public void ParseLogs_WithMalformedLines_ShouldIgnoreThem()
-    {
-        // Arrange
-        var input = """
-            pipeline1 msg1 0 [Valid] END
-            This is not a valid log line
-            Another invalid line
-            pipeline2 msg1 0 [Also valid] END
-            """;
-
-        // Act
-        var result = _parser.ParseLogs(input);
-
-        // Assert
-        result.Should().Contain("Pipeline pipeline1");
-        result.Should().Contain("Pipeline pipeline2");
-        result.Should().Contain("Valid");
-        result.Should().Contain("Also valid");
-    }
-
-    [Fact]
-    public void ParseLogs_WithOrphanedMessages_ShouldHandleGracefully()
-    {
-        // Arrange - msg2 references msg3 which doesn't exist
-        var input = """
-            pipeline1 msg1 0 [First] msg2
-            pipeline1 msg2 0 [Second] msg3
-            """;
-
-        // Act
-        var result = _parser.ParseLogs(input);
-
-        // Assert
-        result.Should().Contain("Pipeline pipeline1");
-        result.Should().Contain("msg1| First");
-        result.Should().Contain("msg2| Second");
-    }
-
-    [Fact]
-    public void ParseLogs_WithCircularReferences_ShouldNotInfiniteLoop()
-    {
-        // Arrange - This creates a circular reference
-        var input = """
-            pipeline1 msg1 0 [First] msg2
-            pipeline1 msg2 0 [Second] msg1
-            """;
-
-        // Act & Assert - Should complete without hanging
-        var result = _parser.ParseLogs(input);
-        result.Should().Contain("Pipeline pipeline1");
-    }
-
-    [Theory]
-    [InlineData("48656C6C6F", "Hello")]
-    [InlineData("576F726C64", "World")]
-    [InlineData("54657374", "Test")]
-    public void ParseLogs_WithVariousHexEncodings_ShouldDecodeCorrectly(string hex, string expected)
-    {
-        // Arrange
-        var input = $"pipeline1 msg1 1 [{hex}] END";
-
-        // Act
-        var result = _parser.ParseLogs(input);
-
-        // Assert
-        result.Should().Contain($"msg1| {expected}");
-    }
 }
-
-// The PipelineParser class is now in its own file: PipelineParser.cs
